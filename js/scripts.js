2a77afc648ab1fcd9dc06b0c319ab74044c1c8e8
John Murray <heartfeats@gmail.com>
2017-09-25 14:17:19 -0700

    add fullName prototype to 'Contact'

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,10 @@ function Contact(first, last) {
   this.lastName = last;
 }
 
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
 // user interface logic
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
@@ -13,16 +17,14 @@ $(document).ready(function() {
     var inputtedLastName = $("input#new-last-name").val();
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
-    // The line var newContact = new Contact(inputtedFirstName, inputtedLastName) creates a new Contact object by calling the Contact constructor and passing it the inputted data. The variable newContact now refers to that newly created contact object, which knows its own firstName and lastName.
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
-      // If we didn't add last() to $(".contact"), each time a new contact was added, every element with the contact class would show the information of the most recently added contact on click. By adding last(), we only bind the event to most recently-inserted contact.
     });
 
 
